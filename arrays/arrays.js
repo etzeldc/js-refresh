aac88954f6cc325182d084fcd352a924b3ea036b
David Etzel <etzeldc@gmail.com>
2018-01-25 17:30:38 -0700

    More Basic Stuff
    Arrays, and a bunch of array methodology

---
+++ arrays/arrays.js
@@ -0,0 +1,101 @@
+// Declares an Implicit Literal Array
+var myArrayA = ["one", 1, "Two", 2, 55, "four"]; // implicit literal array
+// console.log(myArrayA[0]); ...logs first item
+console.log(myArrayA);
+
+// Declares a new Array...less preferrable fashion
+var myArrayB = new Array("one", "two", 55, "four");
+// console.log(myArrayB[2]); ...logs third item
+console.log(myArrayB);
+
+// Declares a blank Array with X slots
+var myArrayC = new Array(5); // creates five empty slots
+console.log(myArrayC);
+
+var myArrayD = ["xone", "xa", 3, "two", 545, "xfour"];
+console.log(myArrayD);
+
+// PUSH to an Array (adds to end of Array)
+document.getElementById('btn1').addEventListener('click', function () {
+    myArrayA.push("five");
+    console.log(myArrayA);
+    myArrayB.push("five");
+    console.log(myArrayC);
+    myArrayC.push("five");
+    console.log(myArrayC);
+});
+
+// Find LENGTH of an Array
+document.getElementById('btn2').addEventListener('click', function () {
+    console.log(myArrayA, myArrayA.length);
+    console.log(myArrayB, myArrayB.length);
+    console.log(myArrayC, myArrayC.length);
+});
+
+// POP from an Array (takes away from end of Array)
+document.getElementById('btn3').addEventListener('click', function () {
+    myArrayA.pop();
+    console.log(myArrayA);
+    myArrayB.pop();
+    console.log(myArrayB);
+    myArrayC.pop();
+    console.log(myArrayC);
+});
+
+// SHIFT an Array (takes away first item of Array, shifts the rest)
+document.getElementById('btn4').addEventListener('click', function () {
+    myArrayA.shift();
+    console.log(myArrayA);
+    myArrayB.shift();
+    console.log(myArrayB);
+    myArrayC.shift();
+    console.log(myArrayC);
+});
+
+// INDEX OF an Array (searches for a specific value's position within an Array)
+    // (if value isn't present, -1 is returned)
+document.getElementById('btn5').addEventListener('click', function () {
+    console.log(myArrayA.indexOf("four"));
+    console.log(myArrayB.indexOf("four"));
+    console.log(myArrayC.indexOf("four"));
+});
+
+// SPLICE an Array (removes multiple items from Array)
+document.getElementById('btn6').addEventListener('click', function () {
+    myArrayA.splice(2, 1); // ...splice.(starting position, # of items to remove)
+    console.log(myArrayA);
+    myArrayB.splice(2, 1);
+    console.log(myArrayB);
+    myArrayC.splice(2, 1);
+    console.log(myArrayC);
+});
+
+// UNSHIFT an Array (adds specific value/s to beginning of Array, shifts the rest)
+document.getElementById('btn7').addEventListener('click', function () {
+    myArrayA.unshift("THREE");
+    console.log(myArrayA);
+    myArrayB.unshift("THREE");
+    console.log(myArrayB);
+    myArrayC.unshift("THREE");
+    console.log(myArrayC);
+});
+
+// SORT an Array (orders the items by increasing numbers, then alphabetically with uppercases first)
+document.getElementById('btn8').addEventListener('click', function() {
+    console.log(myArrayA.sort());
+    console.log(myArrayB.sort());
+    console.log(myArrayC.sort());
+});
+
+// REVERSE an Array (orders the items by alphabet backwards with lowercases first, then decreasing numbers)
+document.getElementById('btn9').addEventListener('click', function() {
+    console.log(myArrayA.reverse());
+    console.log(myArrayB.reverse());
+    console.log(myArrayC.reverse());
+});
+
+// CONCATENATE an Array (basically 'pushes' an entire Array to the end of another)
+var myArrayE = myArrayA.concat(myArrayD);
+document.getElementById('btn10').addEventListener('click', function () {
+    console.log(myArrayE);
+});
--- intro-basics/intro-basics.js
+++ intro-basics/intro-basics.js
@@ -1,1 +1,32 @@
-alert('sup yo?');
+//alert('sup yo?');
+
+// Alert Button Three JS
+document.getElementById('btn3').addEventListener('click', function() {
+    alert('hello 3');
+});
+
+
+// Prompt Button One JS
+document.getElementById('btn4').addEventListener('click', function() {
+    prompt('what is your name?');
+});
+
+// Prompt Button Two JS
+    // to store a response, set it to a variable and log that variable
+document.getElementById('btn5').addEventListener('click', function() {
+    var myName = prompt('what is your name?');
+    console.log(myName);
+});
+
+// Prompt Button Three JS
+    // note var myName above is local to that function...
+    // while var otherName is global so it can be used by separate functions
+var otherName = '';
+document.getElementById('btn6').addEventListener('click', function () {
+    otherName = prompt('what is your other name?');
+});
+
+// Prompt Button Four JS
+document.getElementById('btn7').addEventListener('click', function () {
+    console.log(otherName);
+});
