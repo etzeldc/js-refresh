0f431c9131e9054ae7b115f4fdc1a4a2223040cf
David Etzel <etzeldc@gmail.com>
2018-01-26 15:42:35 -0700

    More Object basics
    Second Objects lesson

--- objects/objects.js
+++ objects/objects.js
@@ -7,7 +7,7 @@ document.getElementById('btn1').addEventListener('click', function() {
     console.log(myObject);
 });
 
-// Declares an Implicit Literal Object
+// Initializes an Implicit Literal Object
 var myObjectA = {
     firstName: 'Dave',  // {"property: value", "property: value"}
     company: 'Uber',
@@ -21,4 +21,16 @@ document.getElementById('btn2').addEventListener('click', function() {
 document.getElementById('btn3').addEventListener('click', function () {
     console.log(myObjectA['firstName']); // uses a string of a property name like an index
     console.log(myObjectA.firstName); // more dynamic, but same effect
+});
+
+// Initializes an object using a function
+var myObjectB = new myInfo('David', 'Lyft', 31); // sets variable object to a initializing function (w/ 3 args)
+function myInfo(a, b, c) {
+    this.firstName = a; // "this" is specific to the object calling this function
+    this.company = b;
+    this.age = c;
+};
+document.getElementById('btn4').addEventListener('click', function () {
+    console.log(myObjectB);
+    console.log(myObjectB.age);
 });
