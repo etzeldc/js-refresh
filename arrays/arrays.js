9c2d801718ed4aae672285f101293a41bcfbebc3
David Etzel <etzeldc@gmail.com>
2018-01-25 17:55:46 -0700

    More Basics
    Rest of the Array methodologies

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -62,7 +62,7 @@ document.getElementById('btn5').addEventListener('click', function () {
 
 // SPLICE an Array (removes multiple items from Array)
 document.getElementById('btn6').addEventListener('click', function () {
-    myArrayA.splice(2, 1); // ...splice.(starting position, # of items to remove)
+    myArrayA.splice(2, 1); // "...splice(starting position, # of items to remove)"
     console.log(myArrayA);
     myArrayB.splice(2, 1);
     console.log(myArrayB);
@@ -98,4 +98,27 @@ document.getElementById('btn9').addEventListener('click', function() {
 var myArrayE = myArrayA.concat(myArrayD);
 document.getElementById('btn10').addEventListener('click', function () {
     console.log(myArrayE);
+});
+
+// Arrays into STRINGS
+document.getElementById('btn11').addEventListener('click', function () {
+    console.log(myArrayA.toString());
+    console.log(myArrayB.toString());
+    console.log(myArrayC.toString());
+    console.log(myArrayD.toString());
+});
+
+// Arrays into STRINGS W/ SEPARATORS
+document.getElementById('btn12').addEventListener('click', function () {
+    console.log(myArrayA.join(" :) ")); // ...adds ' :) ' between each item in the string
+    console.log(myArrayB.join(" :) "));
+    console.log(myArrayC.join(" :) "));
+    console.log(myArrayD.join(" :) "));
+});
+
+// SLICE an Array (returns a segment of an Array based on specified indexes)
+document.getElementById('btn13').addEventListener('click', function () {
+    console.log(myArrayE);
+    console.log(myArrayE.slice(2,5)); // "...slice(starting postion, stopping position)"
+    // Slice doesnt include stopping point, so this returns values of indexes 2-4
 });
