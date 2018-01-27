4673261152d48aeba1675c4a7ed230a18bda5e51
David Etzel <etzeldc@gmail.com>
2018-01-26 17:06:49 -0700

    Mo Func
    Lesson two of three

--- functions/functions.js
+++ functions/functions.js
@@ -27,3 +27,41 @@ document.getElementById('btn3').addEventListener('click', function () {
     myFunction3('gizmo');
     myFunction3();
 });
+
+// Calculations
+function myCalculation(a) {
+    return (a + b);
+}
+document.getElementById('btn4').addEventListener('click', function () {
+    console.log(myCalculation(45));
+    console.log(myCalculation(13));
+    console.log(myCalculation(72));
+    console.log(myCalculation(8));
+});
+
+// Calculations with extra values
+function myCalculationX(a, c) {
+    var d = a + b + c;
+    b++;
+    return (a * c) + " " + d;
+}
+document.getElementById('btn5').addEventListener('click', function () {
+    console.log(myCalculationX(4, 3));
+    console.log(myCalculationX(13, 2));
+    console.log(myCalculationX(21, 5));
+    console.log(myCalculationX(7, 7));
+});
+
+// Functions to update Objects
+var myObj = { // creates an implicitly literal object
+    firstName: "David",
+    lastName: "Etzel"
+};
+function  f(g) { // argument 'g' (the object) will pass through and be updated
+    g.courses = 'JavaScript'; //updated by adding a new property
+}
+document.getElementById('btn6').addEventListener('click', function () {
+    console.log(myObj);
+    f(myObj);
+    console.log(myObj);
+});   
